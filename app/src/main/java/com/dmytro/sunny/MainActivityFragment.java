ba240141d2119024ce63c524fec01f5aeaf64845
dmytrobohdanov <dmytro.bohdanov@gmail.com>
2016-06-04 18:07:03 +0300

    up

--- app/src/main/java/com/dmytro/sunny/MainActivityFragment.java
+++ app/src/main/java/com/dmytro/sunny/MainActivityFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,14 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        ArrayList<String> fakeData = new ArrayList<>();
+        for(int i = 0; i <= 15; i++) {
+            fakeData.add("SomeWeather - " + (i + 10));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.list_item_forecast, R.id.list_item_forecast_textview,fakeData);
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
